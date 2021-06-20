0a88ec71f4168fea8dcf2b77eadb22da4a45e027
scriptndestroy <ojedamaruny@gmail.com>
2021-06-20 19:16:24 +0200

    Swagger

--- CiklumTasks.API/Startup.cs
+++ CiklumTasks.API/Startup.cs
@@ -63,9 +63,11 @@ namespace CiklumTasks.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CiklumTaskAPI v1"));
-            }            
+                
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CiklumTaskAPI v1"));
 
             app.UseHttpsRedirection();
 
